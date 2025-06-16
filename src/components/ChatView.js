032d876c776fca0ef592ef072ac415d40bc58ad3
JulioGmz89 <a01274966@itesm.mx>
2025-06-16 13:46:26 -0600

    feat: added icon and a better microphone icon

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -283,7 +283,7 @@ const ChatView = ({ chat, conversation, onBack, onEditGroup }) => {
             </button>
           ) : (
             <button type="button" aria-label="Record audio" className="p-3 rounded-full bg-my-message-bg-light dark:bg-my-message-bg-dark text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-accent-light dark:focus-visible:ring-accent-dark">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M19 11a7 7 0 01-14 0m7 6v4m0 0H9m4 0h2M5 11a7 7 0 0114 0" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M12 1a3 3 0 00-3 3v8a3 3 0 006 0V4a3 3 0 00-3-3zM19 10v2a7 7 0 01-14 0v-2" /></svg>
             </button>
           )}
         </div>
